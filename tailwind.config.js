41e3d7064595734d109edbcecf8736601a4ded00
tylerlavoie <lavoie.works@gmail.com>
2023-11-22 12:03:56 -0500

    fixing header and first text component

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,15 @@ module.exports = {
   purge: ['./public/**/*.html', './src/**/*.{astro,js,jsx,ts,tsx,vue}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    container: {
+      padding: {
+        DEFAULT: '2rem',
+        sm: '3rem',
+        md: '4rem',
+        lg: '4rem',
+        xl: '5rem',
+      },
+    },
     extend: {
       colors: {
         'NFTA-navy': '#002169',
