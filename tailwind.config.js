ba9c30a5c4dcab247251ea7ee74ab307152a8c8f
tylerlavoie <lavoie.works@gmail.com>
2023-11-22 11:21:08 -0500

    setting up default tailwind styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@ module.exports = {
   purge: ['./public/**/*.html', './src/**/*.{astro,js,jsx,ts,tsx,vue}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'NFTA-navy': '#002169',
+        'NFTA-silver': '#888C8D',
+        'NFTA-teal': '#2FBCCE'
+      },
+      fontFamily: {
+        heading: ['Poppins', 'sans-serif'],
+        body: ['Inter', 'sans-serif']
+      }
+    },
   },
   variants: {
     extend: {},
